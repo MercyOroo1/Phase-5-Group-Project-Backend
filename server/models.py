b6ab5d0ac8a9ec59926e41031c13d261c66e17d0
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-07-31 01:14:10 +0300

    add reset password route

--- server/models.py
+++ server/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
 
 db=SQLAlchemy()
 
@@ -10,72 +12,88 @@ class User(db.Model):
     full_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
-    role = db.relationship('Role',backpopulates='role')
+    reset_token = db.Column(db.String, nullable = True)
+    token_expiry = db.Column(db.String, nullable = True)
+    password = db.Column(db.String, nullable=False)
+    reset_token = db.Column(db.String, nullable = True)
+    token_expiry = db.Column(db.String, nullable = True)
 
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-    agents=db.relationship('Agent', backpopulates='user')
-    properties = db.relationship('Property', backref=db.backref('user'))
-    savedproperties=db.relationship('SavedProperty', backpopulates='user')
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id', name='fk_user_role'))
+    # created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable = True)
+    # updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp(), nullable = True)
+    agents = db.relationship('Agent', back_populates='user')
+    properties = db.relationship('Property', back_populates='user')
+    saved_properties = db.relationship('SavedProperty', back_populates='user')
+    reviews = db.relationship('Review', back_populates='user')
+    contact_messages = db.relationship('ContactMessage', back_populates='user')
+    role = db.relationship('Role', back_populates='users')
+    
+    
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
     users = db.relationship('User', back_populates='role')
-
-
 
 class Property(db.Model):
     __tablename__ = 'properties'
     id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String,nullable=False)
+    address = db.Column(db.String, nullable=False)
     city = db.Column(db.String, nullable=False)
-    squareFootage = db.Column(db.Integer, nullable=False)
+    square_footage = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Integer, nullable=False)
+    property_type = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    photos=db.relationship('Photo',backpopulates='property')
-    user = db.relationship('User', backref=db.backref('properties'))
-    agent = db.relationship('Agent', backref=db.backref('property'))
-    saved_by = db.relationship('User', backref=db.backref('saved_properties'))
-    contact_messages=db.relationship('ContactMessage', backref=db.backref('property'))
 
-class Photo(db.Models):
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_property_user'), nullable=False)
+    agent_id = db.Column(db.Integer, db.ForeignKey('agents.id', name='fk_property_agent'), nullable=True)
+
+    photos = db.relationship('Photo', back_populates='property')
+    agent = db.relationship('Agent', back_populates='properties')
+    saved_by = db.relationship('SavedProperty', back_populates='property')
+    contact_messages = db.relationship('ContactMessage', back_populates='property')
+    reviews = db.relationship('Review', back_populates='property')
+    user = db.relationship('User', back_populates='properties')
+
+class Photo(db.Model):
     __tablename__ = 'photos'
     id = db.Column(db.Integer, primary_key=True)
     photo_url = db.Column(db.String, nullable=False)
-    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
-    property = db.relationship('Property', backref=db.backref('photos'))
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id', name='fk_photo_property'), nullable=False)
+    property = db.relationship('Property', back_populates='photos')
 
-
-class Agent(db.Models):
+class Agent(db.Model):
     __tablename__ = 'agents'
     id = db.Column(db.Integer, primary_key=True)
-    license_number = db.Column(db.Integer, nullable=False)
+    license_number = db.Column(db.String, nullable=False)
     full_name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
     experience = db.Column(db.String, nullable=False)
     phone_number = db.Column(db.String, nullable=False)
     for_sale = db.Column(db.Integer, nullable=False)
-    sold=db.Column(db.Integer, nullable=False)
+    sold = db.Column(db.Integer, nullable=False)
     languages = db.Column(db.String, nullable=False)
     agency_name = db.Column(db.String, nullable=False)
-    listed_proprties = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('agents'))
-    properties = db.relationship('Property', backref=db.backref('agent')) 
+    listed_properties = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_agent_user'), nullable=False)
 
+    user = db.relationship('User', back_populates='agents')
+    properties = db.relationship('Property', back_populates='agent')
 
-
-class SavedProperty(db.Models):
-    __tablename__ ='saved_properties'
+class SavedProperty(db.Model):
+    __tablename__ = 'saved_properties'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('saved_properties'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_savedproperty_user'), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id', name='fk_savedproperty_property'), nullable=False)
+
+    user = db.relationship('User', back_populates='saved_properties')
+    property = db.relationship('Property', back_populates='saved_by')
 
 class ContactMessage(db.Model):
     __tablename__ = 'contact_messages'
@@ -85,21 +103,106 @@ class ContactMessage(db.Model):
     subject = db.Column(db.String, nullable=False)
     message = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
-    property = db.relationship('Property', backref=db.backref('contact_messages'))
 
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id', name='fk_contactmessage_property'), nullable=False)
+    property = db.relationship('Property', back_populates='contact_messages')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_contactmessage_user'), nullable=False)
+    user = db.relationship('User', back_populates='contact_messages')
 
-
-class Reviews(db.Model):
-    __tablename__ ='reviews'
+class Review(db.Model):
+    __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
-    property = db.relationship('Property', backref=db.backref('reviews'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('reviews'))
-    agent_id = db.Column(db.Integer, db.ForeignKey('agents.id'), nullable=False)
+
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id', name='fk_review_property'), nullable=False)
+    property = db.relationship('Property', back_populates='reviews')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_review_user'), nullable=False)
+    user = db.relationship('User', back_populates='reviews')
+
+
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     users = db.relationship('User', back_populates='role')
+
+
+
+# class Property(db.Model):
+#     __tablename__ = 'properties'
+#     id = db.Column(db.Integer, primary_key=True)
+#     address = db.Column(db.String,nullable=False)
+#     city = db.Column(db.String, nullable=False)
+#     squareFootage = db.Column(db.Integer, nullable=False)
+#     price = db.Column(db.Integer, nullable=False)
+#     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+#     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     photos=db.relationship('Photo',back_populates='property')
+#     user = db.relationship('User', backref=db.backref('properties'))
+#     agent = db.relationship('Agent', backref=db.backref('property'))
+#     saved_by = db.relationship('User', backref=db.backref('saved_properties'))
+#     contact_messages=db.relationship('ContactMessage', backref=db.backref('property'))
+
+# class Photo(db.Model):
+#     __tablename__ = 'photos'
+#     id = db.Column(db.Integer, primary_key=True)
+#     photo_url = db.Column(db.String, nullable=False)
+#     property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+#     property = db.relationship('Property', backref=db.backref('photos'))
+
+
+# class Agent(db.Model):
+#     __tablename__ = 'agents'
+#     id = db.Column(db.Integer, primary_key=True)
+#     license_number = db.Column(db.Integer, nullable=False)
+#     full_name = db.Column(db.String, nullable=False)
+#     email = db.Column(db.String, nullable=False)
+#     experience = db.Column(db.String, nullable=False)
+#     phone_number = db.Column(db.String, nullable=False)
+#     for_sale = db.Column(db.Integer, nullable=False)
+#     sold=db.Column(db.Integer, nullable=False)
+#     languages = db.Column(db.String, nullable=False)
+#     agency_name = db.Column(db.String, nullable=False)
+#     listed_proprties = db.Column(db.String, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     user = db.relationship('User', backref=db.backref('agents'))
+#     properties = db.relationship('Property', backref=db.backref('agent')) 
+
+
+
+# class SavedProperty(db.Model):
+#     __tablename__ ='saved_properties'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+#     user = db.relationship('User', backref=db.backref('saved_properties'))
+
+# class ContactMessage(db.Model):
+#     __tablename__ = 'contact_messages'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     email = db.Column(db.String, nullable=False)
+#     subject = db.Column(db.String, nullable=False)
+#     message = db.Column(db.String, nullable=False)
+#     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+#     property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+#     property = db.relationship('Property', backref=db.backref('contact_messages'))
+
+
+
+# class Reviews(db.Model):
+#     __tablename__ ='reviews'
+#     id = db.Column(db.Integer, primary_key=True)
+#     rating = db.Column(db.Integer, nullable=False)
+#     comment = db.Column(db.String, nullable=False)
+#     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+#     property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+#     property = db.relationship('Property', backref=db.backref('reviews'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     user = db.relationship('User', backref=db.backref('reviews'))
+#     agent_id = db.Column(db.Integer, db.ForeignKey('agents.id'), nullable=False)
  
    
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,81 +1,115 @@
-from flask import Blueprint, jsonify,session
+from flask import Blueprint, jsonify, request,session
 from flask_restful import Api, Resource, reqparse
 from models import User, db
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import create_access_token, JWTManager, create_refresh_token, jwt_required, current_user
-from functools import wraps
+from werkzeug.security import generate_password_hash
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+from flask_mail import Message
+from datetime import datetime, timedelta
 
-
+serializer = URLSafeTimedSerializer('We are winners')
 
 auth_bp = Blueprint('auth_bp', __name__, url_prefix='/auth')
 auth_api = Api(auth_bp)
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
-
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return User.query.filter_by(id=identity).first()
 
-
 register_args = reqparse.RequestParser()
-register_args.add_argument('full_name',type=str, required=True, help='Full name is required')
-register_args.add_argument('email',type=str, required=True, help='email name is required')
-register_args.add_argument('password',type=str, required=True, help='password is required')
-register_args.add_argument('password2',type=str, required=True, help='Please enter your password again')
-
+register_args.add_argument('full_name', type=str, required=True, help='Full name is required')
+register_args.add_argument('email', type=str, required=True, help='Email is required')
+register_args.add_argument('password', type=str, required=True, help='Password is required')
+register_args.add_argument('password2', type=str, required=True, help='Please enter your password again')
 
 login_args = reqparse.RequestParser()
 login_args.add_argument('email')
 login_args.add_argument('password')
 
+class ResetPasswordRequest(Resource):
+    def __init__(self, mail):
+        self.mail = mail
+    
+    def post(self):
+        data = request.get_json()
+        email = data.get('email')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            token = serializer.dumps(email, salt='reset-password')
+            user.reset_token = token
+            # user.token_expiry = datetime.utcnow() + timedelta(hours=1)
+            reset_url = f"http://127.0.0.1:5000/auth/reset-password?token={token}"
+            msg = Message("Password Reset Request",
+                          sender="mercy.oroo.ke@gmail.com",
+                          recipients=[email])
+            msg.body = f"Use this link to reset your password: {reset_url}"
+            self.mail.send(msg)
 
+        return jsonify({'message': 'If the email exists, a reset link has been sent.'})
+    
+class ResetPassword(Resource):
+    def post(self):
+        data = request.get_json()
+        token = data.get('token')
+        new_password = data.get('password')
+        
+        try:
+            email = serializer.loads(token, salt='reset-password', max_age=3600)
+        except SignatureExpired:
+            return jsonify({'message': 'The reset link is invalid or expired.'}), 400
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            user.password = bcrypt.generate_password_hash(new_password)
+            db.session.commit()
+            return jsonify({'message': 'Password has been reset.'})
+        return jsonify({'message': 'User not found.'}), 404
 
 class Register(Resource):
-
     def post(self):
         data = register_args.parse_args()
         # hash the password
         if data.get('password') != data.get('password2'):
             return {"msg": "Passwords do not match"}
         hashed_password = bcrypt.generate_password_hash(data.get('password'))
-        new_user = User(full_name = data.get('full_name'),email = data.get('email'),password = hashed_password )
+        new_user = User(full_name=data.get('full_name'), email=data.get('email'), password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-
-
-        return {"msg": "user registration successful"}
+        return {"msg": "User registration successful"}
     
-
 class Login(Resource):
-      def post(self):
+    def post(self):
         data = login_args.parse_args()
-
-        user = User.query.filter_by(email = data.get('email')).first()
+        user = User.query.filter_by(email=data.get('email')).first()
 
         if not user:
-            return {"msg":"User does not Exist"}
-        if not bcrypt.check_password_hash(user.password,data.get('password')):
-            return {"msg":"Passwords Does not Exit"}
+            return {"msg": "User does not exist"}
+        if not bcrypt.check_password_hash(user.password, data.get('password')):
+            return {"msg": "Password does not match"}
 
         token = create_access_token(identity=user.id)
         refresh_token = create_refresh_token(identity=user.id)
+        return {"token": token, "refresh_token": refresh_token}
 
-        return {"token":token,"refresh_token":refresh_token}
-
-
-      @jwt_required(refresh=True)
-      def get(self):
-        token = create_access_token(identity= current_user.id )
-        return {"token":token}
+    @jwt_required(refresh=True)
+    def get(self):
+        token = create_access_token(identity=current_user.id)
+        return {"token": token}
       
-
 class Logout(Resource):
     def delete(self):
         session.pop('user_id', None)
-        return {'msg': 'You have successfully logged out'},200
-    
-auth_api.add_resource(Register, '/register')
-auth_api.add_resource(Login, '/login')
-auth_api.add_resource(Logout, '/logout')
+        return {'msg': 'You have successfully logged out'}, 200
+
+# Register resources
+def create_resources(mail):
+    # Initialize resources with the necessary dependencies
+    auth_api.add_resource(Register, '/register')
+    auth_api.add_resource(Login, '/login')
+    auth_api.add_resource(Logout, '/logout')
+    auth_api.add_resource(ResetPasswordRequest, '/reset-password-request', resource_class_kwargs={'mail': mail})
+    auth_api.add_resource(ResetPassword, '/reset-password')
