76788e1ffc46ab347f9e6dab71d30d5af64e5750
kamusarihanshi <ahmedizaka9@gmail.com>
2024-07-30 08:46:48 +0300

    main.py changed

---
+++ server/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+
+
+
+
+
+
+app = Flask(__name__)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def home():
+    return "Welcome to the Home Page!"
+
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Galaxy.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5050,debug=True)
