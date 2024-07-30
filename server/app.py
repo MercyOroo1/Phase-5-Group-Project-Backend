2fc71b3467ac6f6a51dc9edb1d0f29afc48d35f2
kamusarihanshi <ahmedizaka9@gmail.com>
2024-07-30 10:20:40 +0300

    new models included

--- server/app.py
+++ server/app.py
@@ -10,6 +10,9 @@ from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///property.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app = app, db= db)
 
 migrate = Migrate(app = app, db= db)
 db.init_app(app)
