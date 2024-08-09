9aa8b18cc225d469422a0da0ddb15269fe4acd50
Mercy Oroo <162958349+MercyOroo1@users.noreply.github.com>
2024-08-09 12:26:14 +0300

    testing

--- server/app.py
+++ server/app.py
@@ -34,7 +34,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'We are winners' 
 jwt = JWTManager(app)
 
-
+# hello
 app.register_blueprint(auth_bp)
 app.register_blueprint(agent_bp)
 app.register_blueprint(saved_bp)
