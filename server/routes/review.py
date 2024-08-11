98500d0c56abfb2bca6da67a0e667caec5be0b28
geraldkisilu <muligerald6@gmail.com>
2024-08-11 23:19:07 +0300

    commit

--- server/routes/review.py
+++ server/routes/review.py
@@ -1,35 +1,23 @@
-from flask import Blueprint
-from flask_restful import Api, Resource, reqparse
-from models import Review, db
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask_cors import CORS
+from models import Review, db
 
-review_bp = Blueprint("reviews", __name__, url_prefix="/reviews")
-review_api = Api(review_bp)
+review_bp = Blueprint('reviews', __name__, url_prefix='/reviews')
 
-CORS(review_bp)
+@review_bp.route('/gotten', methods=['POST'])
+@jwt_required()
+def add_review():
+    data = request.get_json()
+    user_id = get_jwt_identity()
 
+    new_review = Review(
+        property_id=data['property_id'],
+        user_id=user_id,
+        rating=data['rating'],
+        comment=data['comment']
+    )
 
-review_parser = reqparse.RequestParser()
-review_parser.add_argument("property_id", type=int, required=True, help="Property ID is required")
-review_parser.add_argument("rating", type=int, required=True, help="Rating is required")
-review_parser.add_argument("comment", type=str, required=True, help="Comment is required")
+    db.session.add(new_review)
+    db.session.commit()
 
-class ReviewResource(Resource):
-    @jwt_required()
-    def post(self):
-        args = review_parser.parse_args()
-        current_user_id = get_jwt_identity()  
-
-        new_review = Review(
-            property_id=args['property_id'],
-            user_id=current_user_id,
-            rating=args['rating'],
-            comment=args['comment']
-        )
-        
-        db.session.add(new_review)
-        db.session.commit()
-        return {'message': 'Review added successfully'}, 201
-
-review_api.add_resource(ReviewResource, '/gotten')
+    return jsonify({'message': 'Review added successfully'}), 201
